531aa5440c2b7e342e5bb49d3be89c06cabe70c8
Vinayak Negi <95993782+Vinayaknegi25@users.noreply.github.com>
2022-11-17 10:55:48 +0530

    Create 374_Guess_Number_Higher_or_Lower.cpp

---
+++ 374_Guess_Number_Higher_or_Lower.cpp
@@ -0,0 +1,29 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) 
+    {
+        int beg=0,end=n;
+        int mid;
+        while(beg<=end)
+        {
+            mid=beg +(end-beg)/2;
+            if(guess(mid) == 0 )
+                return mid;
+            else if(guess(mid) == 1)
+                beg=mid+1;
+            else
+                end=mid-1;
+        }
+        return mid;
+            
+    }
+};
