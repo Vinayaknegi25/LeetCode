cbe221b20a6348a1946e4cb58cedbbeea6100829
Vinayak Negi <95993782+Vinayaknegi25@users.noreply.github.com>
2022-11-30 14:02:12 +0530

    Create 2485_Find_the_Pivot_Integer.cpp

---
+++ 2485_Find_the_Pivot_Integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotInteger(int n) 
+    {
+        if(n==1)
+            return n;
+        int sum1=0,sum2=0;
+        for(int i=1;i<n;i++)
+        {
+            sum1=sum1+i;
+            sum2=0;
+            for(int j=i;j<=n;j++)
+            {
+                sum2=sum2+j;
+            }
+            if(sum1==sum2)
+                return i;
+        }
+        return -1;
+
+    }
+};
