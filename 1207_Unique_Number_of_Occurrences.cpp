1eee3633d328e335652cbfd72f8610a828192639
Vinayak Negi <95993782+Vinayaknegi25@users.noreply.github.com>
2022-11-30 13:10:25 +0530

    Create 1207_Unique_Number_of_Occurrences.cpp

---
+++ 1207_Unique_Number_of_Occurrences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) 
+    {
+        unordered_map<int,int> m;
+        unordered_set<int> s;
+        for(int i : arr)
+            m[i]++;
+        for (auto& j : m) s.insert(j.second); 
+        return (m.size() == s.size());
+    }
+};
