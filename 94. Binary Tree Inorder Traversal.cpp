8f7f520b1ed7ac93e1cb4d16368c2dc50a5c616d
Vinayak Negi <95993782+Vinayaknegi25@users.noreply.github.com>
2023-02-14 08:48:52 +0530

    Create 94. Binary Tree Inorder Traversal.cpp

---
+++ 94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    void InOrder(TreeNode* root,vector<int> &v)
+    {
+        if(root==NULL)
+            return;
+        InOrder(root->left,v);
+        v.push_back(root->val);
+        InOrder(root->right,v);
+    }
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+       vector<int> v;
+       InOrder(root,v);
+       return v;
+    }
+};
