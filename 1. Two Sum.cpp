31b594b477c2158e4c5dc4598a55bc402baf967a
Vinayak Negi <95993782+Vinayaknegi25@users.noreply.github.com>
2023-05-23 09:07:36 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,3 +1,4 @@
+// Brute Force
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -16,3 +17,20 @@ public:
         return {a,b};
     }
 };
+
+// Optimised Aproach
+class Solution{
+    public:
+    vector<int> twoSum(vector<int>& nums, int target){
+        map<int,int> map;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(map.find(target-nums[i])!=map.end())
+            {
+                return {map[targrt-nums[i],i};
+            }
+            map[nums[i]]=i;
+        }
+        return {};
+    }
+}
