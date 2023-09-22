55ed93770feffd7b59673548a694995fe71652ee
Vinayak Negi <95993782+Vinayaknegi25@users.noreply.github.com>
2023-09-22 09:55:41 +0530

    Update 75_Sort_Colors.cpp (Added 2nd method)

--- 75_Sort_Colors.cpp
+++ 75_Sort_Colors.cpp
@@ -15,3 +15,30 @@ public:
         
     }
 };
+
+Approach 2 (Dutch national flag approach)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[high],nums[mid]);
+                high--;
+            }
+        }
+    }
+};
